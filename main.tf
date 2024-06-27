0e7512a4b52599e05f44fd391d6ef59e7075f5fb
Saul Ramos <seramos@cleveritgroup.com>
2024-06-27 17:06:33 -0400

    minor fix main.tf

--- main.tf
+++ main.tf
@@ -8,12 +8,7 @@ provider "azurerm" {
 }
 
 terraform {
-  backend "azurerm" {
-    resource_group_name  = var.backend_resource_group_name
-    storage_account_name = var.backend_storage_account_name
-    container_name       = var.backend_container_name
-    key                  = var.backend_key
-  }
+  backend "azurerm" {}
 }
 resource "azurerm_resource_group" "demo" {
   name     = var.azurerm_resource_group_name
